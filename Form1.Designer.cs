b47176a000b0c7fd5f8c95e72b72e53d934c0758
Luu Tuan Hung <hung2a10@gmail.com>
2025-04-24 18:34:09 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Lab8_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btAnswer = new System.Windows.Forms.Button();
+            this.lbAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Пойти сегодня в университет?";
+            // 
+            // btAnswer
+            // 
+            this.btAnswer.Location = new System.Drawing.Point(340, 105);
+            this.btAnswer.Name = "btAnswer";
+            this.btAnswer.Size = new System.Drawing.Size(75, 23);
+            this.btAnswer.TabIndex = 1;
+            this.btAnswer.Text = "ответ";
+            this.btAnswer.UseVisualStyleBackColor = true;
+            this.btAnswer.Click += new System.EventHandler(this.btAnswer_Click);
+            // 
+            // lbAnswer
+            // 
+            this.lbAnswer.AutoSize = true;
+            this.lbAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAnswer.Location = new System.Drawing.Point(367, 144);
+            this.lbAnswer.Name = "lbAnswer";
+            this.lbAnswer.Size = new System.Drawing.Size(0, 29);
+            this.lbAnswer.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbAnswer);
+            this.Controls.Add(this.btAnswer);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btAnswer;
+        private System.Windows.Forms.Label lbAnswer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab8_1
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        private double p;
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+            p = 0.5;
+        }
+
+        private void btAnswer_Click(object sender, EventArgs e)
+        {
+            double alpha = random.NextDouble(); // [0.0, 1.0)
+
+            if (alpha < p)
+            {
+                lbAnswer.Text = "ДА!"; 
+            }
+            else
+            {
+                lbAnswer.Text = "НЕТ!"; 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
